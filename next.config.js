263219cc06a2e8e8c298631a962a7385a5943689
Paresh Tarsariya <111181055+DARKfission@users.noreply.github.com>
2025-04-23 02:34:23 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  },
-  // If you're using app router, you might need this
-  experimental: {
-    appDir: true,
   },
 }
 
