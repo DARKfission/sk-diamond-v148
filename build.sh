24a30c7d3eed8b249fe8067569597e0fcd11ae75
Paresh Tarsariya <111181055+DARKfission@users.noreply.github.com>
2025-04-29 23:42:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,8 @@ if [ -f pnpm-lock.yaml ]; then
   rm pnpm-lock.yaml
 fi
 
-# Remove node_modules if it exists
-if [ -d node_modules ]; then
-  rm -rf node_modules
-fi
-
-# Install dependencies with npm
-npm install
+# Install dependencies with pnpm without frozen lockfile
+pnpm install --no-frozen-lockfile
 
 # Build the project
-npm run build
+pnpm run next-build
