912445edaa44f7ad6034458bf9fb4b285b2f906e
Paresh Tarsariya <111181055+DARKfission@users.noreply.github.com>
2025-05-04 16:24:15 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
+module.exports = {
+  target: 'experimental-serverless-trace',
+  webpack: (config, { isServer }) => {
+    // Custom webpack config for Workers
+    if (isServer) {
+      config.target = 'webworker'; // Make sure the server-side code is compatible with Workers
+    }
+    return config;
+  },
+}
 const nextConfig = {
   output: 'export',
   images: {
