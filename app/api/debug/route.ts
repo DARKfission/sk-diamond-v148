6b9a784d7103524995eb2a9a8054352abae10d36
Paresh Tarsariya <111181055+DARKfission@users.noreply.github.com>
2025-05-12 01:28:29 +0200

    Delete app/api/debug/route.ts

--- app/api/debug/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET() {
-  return NextResponse.json({
-    env: process.env.RESEND_API_KEY || "Not found in environment",
-  })
-}
